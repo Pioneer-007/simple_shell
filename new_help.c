cb09cdd289cb7932808f8ae24ef4863c3ea94071
Oseni Sakariyau Oluwadamilare <oseni_sakariyau0008@yahoo.com>
2022-08-11 20:02:04 +0000

    new_help.c

--- new_help.c
+++ new_help.c
@@ -96,4 +96,102 @@ void new_help_exit(vars_t *vars)
 		fd = open(file, O_RDWR);
 
 		s = malloc(300);
+		if (s == NULL)
+		{
+			_puts_error("Fatal Error");
+			return;
+		}
+		while ((r = read(fd, s, 300)) > 0)
+		{
+			r = write(1, s, r);
+			print_message("\n");
+			if (r == -1)
+			{
+				_puts_error("Fatal Error");
+				return;
+			}
+		}
+		free(s);
+		fd = close(fd);
+	}
 
+	else
+		new_help_cd(vars);
+}
+/**
+ * new_help_cd - help builtin command cd
+ * @vars: if command matches a builtin name, text file is sent to stdout
+ * Return: 0 if sucess
+ */
+void new_help_cd(vars_t *vars)
+{
+	char *file;
+	int fd, r;
+	char *s;
+
+	if (_strcmpr(vars->array_tokens[1], "cd") == 0)
+	{
+		file = "/home/shell_test/shelltestenviroment/helpfiles/cd";
+		fd = open(file, O_RDWR);
+
+		s = malloc(300);
+		if (s == NULL)
+		{
+			_puts_error("Fatal Error");
+			return;
+		}
+		while ((r = read(fd, s, 300)) > 0)
+		{
+			r = write(1, s, r);
+			print_message("\n");
+			if (r == -1)
+			{
+				_puts_error("Fatal Error");
+				return;
+			}
+		}
+		free(s);
+		fd = close(fd);
+	}
+
+	else
+		new_help_env(vars);
+}
+/**
+ * new_help_env - help builtin command env
+ * @vars: if command matches a builtin name, text file is sent to stdout
+ * Return: 0 if sucess
+ */
+void new_help_env(vars_t *vars)
+{
+	char *file;
+	int fd, r;
+	char *s;
+
+	if (_strcmpr(vars->array_tokens[1], "env") == 0)
+	{
+		file = "/home/shell_test/shelltestenviroment/helpfiles/env";
+		fd = open(file, O_RDWR);
+
+		s = malloc(300);
+		if (s == NULL)
+		{
+			_puts_error("Fatal Error");
+			return;
+		}
+		while ((r = read(fd, s, 300)) > 0)
+		{
+			r = write(1, s, r);
+			print_message("\n");
+			if (r == -1)
+			{
+				_puts_error("Fatal Error");
+				return;
+			}
+		}
+		free(s);
+		fd = close(fd);
+	}
+	else
+		new_help_history(vars);
+}
