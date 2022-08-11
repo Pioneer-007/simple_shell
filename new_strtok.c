4d8e6f0bd7641a2402a506eb37a80b3d27e8ab60
Oseni Sakariyau Oluwadamilare <oseni_sakariyau0008@yahoo.com>
2022-08-11 19:59:11 +0000

    new_strtok.c

--- new_strtok.c
+++ new_strtok.c
@@ -48,3 +48,58 @@ char *new_strtok(char *str, const char *delim)
 	}
 	token_start = next_token + i;
 	next_token = token_start;
+	for (i = 0; next_token[i] != '\0'; i++)
+	{
+		if (check_if_match(next_token[i], delim) == 1)
+			break;
+	}
+	if (next_token[i] == '\0')
+		next_token = NULL;
+	else
+	{
+		next_token[i] = '\0';
+		next_token = next_token + i + 1;
+		if (*next_token == '\0')
+			next_token = NULL;
+	}
+	return (token_start);
+}
+
+/**
+ * build_path - Combines two strings one representing the path directory and
+ * another representing the command file.
+ * @directory: Represents a directory in the path.
+ * @command: Represents a file in a directory of the path.
+ * Return: Upon success a string representing the full path of a command.
+ * Otherwise NULL.
+ */
+char *build_path(char *directory, char *command)
+{
+	int i, j;
+	int dir_len;
+	int command_len;
+	int len;
+	char *built;
+
+	if (directory == NULL || command == NULL)
+		return (NULL);
+	dir_len = _strlen(directory) + 1;
+	command_len = _strlen(command) + 1;
+	len = dir_len + command_len;
+
+	built = malloc(sizeof(char) * len);
+	if (built == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+	{
+		for (j = 0; directory[j] != '\0'; j++, i++)
+			built[i] = directory[j];
+		built[i] = '/';
+		i++;
+		for (j = 0; command[j] != '\0'; j++, i++)
+			built[i] = command[j];
+	}
+	built[--i] = '\0';
+	return (built);
+}
