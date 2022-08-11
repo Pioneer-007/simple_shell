292b421c1881d5dc4df5aee2237a3a2b393501ea
Oseni Sakariyau Oluwadamilare <oseni_sakariyau0008@yahoo.com>
2022-08-11 20:22:37 +0000

    history.c

--- history.c
+++ history.c
@@ -74,3 +74,79 @@ void new_history(vars_t *vars)
 {
 	history_t *tmp = vars->history;
 	history_t *tmp2 = vars->history;
+	int i = 0, z = 1;
+	unsigned int counter = 0;
+	char *count;
+
+	if (vars->array_tokens[1] != NULL)
+	{
+		prints_error_msg(vars, ": Command not found: ");
+		print_message(vars->array_tokens[1]);
+		print_message("\n");
+		return;
+	}
+	else
+	{
+		while (tmp)
+		{
+			tmp = tmp->next;
+			i++;
+		}
+		while (z < i)
+		{
+			counter++;
+			count = integer_converter(counter);
+			print_message(count);
+			print_message("  ");
+			_puts3(tmp2->str);
+			tmp2 = tmp2->next;
+			z++;
+			free(count);
+		}
+		tmp2 = tmp2->next;
+		vars->invert = tmp2;
+		free_listint(tmp);
+		free_listint(tmp2);
+		return;
+	}
+}
+/*** WRITES STRING TO STDOUT ***/
+/**
+ * _puts3 - writes a string to standard output
+ * @str: string to write
+ *
+ * Return: number of chars printed or -1 on failure
+ */
+ssize_t _puts3(char *str)
+{
+	ssize_t i, len;
+
+	for (i = 0; str[i]; i++)
+		;
+
+	len = write(1, str, i);
+	if (len != i)
+	{
+		perror("Fatal Error");
+		return (-1);
+	}
+	return (len);
+}
+/**
+ *print_message - print a string to standart output
+ * @str: string to print.
+ * Return: void
+ */
+void print_message(char *str)
+{
+	long num, len;
+
+	num = _strlen(str);
+	len = write(STDOUT_FILENO, str, num);
+	if (len != num)
+
+	{
+		perror("fatal error");
+		exit(1);
+	}
+}
