2d99f12a4bdd043723722e1cfa051b5104e73139
Oseni Sakariyau Oluwadamilare <oseni_sakariyau0008@yahoo.com>
2022-08-11 20:36:19 +0000

    builtins2.c

--- builtins2.c
+++ builtins2.c
@@ -7,3 +7,14 @@
 void new_cd(vars_t *vars)
 {
 	int index;
+
+	if (vars->array_tokens[1] == NULL)
+	{
+		index = find_env_index(*vars, "HOME");
+		chdir((vars->env[index]) + 5);
+	}
+	else if (_strcmpr(vars->array_tokens[1], "-") == 0)
+		print_str(vars->array_tokens[1], 0);
+	else
+		chdir(vars->array_tokens[1]);
+}
