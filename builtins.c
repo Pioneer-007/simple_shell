93e3b5db0f7acbd0fd8f1c876ec365d83a43aed6
Oseni Sakariyau Oluwadamilare <oseni_sakariyau0008@yahoo.com>
2022-08-11 20:43:09 +0000

    builtins.c

--- builtins.c
+++ builtins.c
@@ -39,7 +39,7 @@ void new_exit(vars_t *vars)
 {
 
 	int status;
-	/*Si exit tiene argumentos, lo manejamos/
+	/**Si exit tiene argumentos, lo manejamos*/
 	if (_strcmpr(vars->array_tokens[0], "exit") ==
 			0 &&
 		vars->array_tokens[1] != NULL)
@@ -82,5 +82,89 @@ void _env(vars_t *vars)
 		_puts(vars->env[i]);
 		_puts("\n");
 	}
+	vars->status = 0;
+}
+
+/**
+ * new_setenv - create a new environment variable, or edit an existing variable
+ * @vars: pointer to struct of variables
+ *
+ * Return: void
+ */
+void new_setenv(vars_t *vars)
+{
+	char **key;
+	char *var;
+	/** si los argumentos de setenv son errones imprimimos mensaje de error**/
+	if (vars->array_tokens[1] == NULL || vars->array_tokens[2] == NULL)
+	{
+
+		prints_error_msg(vars, ": Incorrect number of arguments\n");
+		vars->status = 2;
+		return;
+	}
+	key = find_key(vars->env, vars->array_tokens[1]);
+	if (key == NULL)
+	{
+		add_key(vars);
+	}
+	else
+	{
+		var = add_value(vars->array_tokens[1], vars->array_tokens[2]);
+		if (var == NULL)
+		{
+			prints_error_msg(vars, NULL);
+			free(vars->buffer);
+			free(vars->array_tokens);
+			free(vars->commands);
+			free_env(vars->env);
+			exit(127);
+		}
+		free(*key);
+		*key = var;
+	}
+	vars->status = 0;
+}
+
+/**
+ * new_unsetenv - remove an environment variable
+ * @vars: pointer to a struct of variables
+ *
+ * Return: void
+ */
+void new_unsetenv(vars_t *vars)
+{
+	char **key, **newenv;
+	unsigned int i, j;
+
+	if (vars->array_tokens[1] == NULL)
+	{
+		prints_error_msg(vars, ": Incorrect number of arguments\n");
+		vars->status = 2;
+		return;
+	}
+	key = find_key(vars->env, vars->array_tokens[1]);
+	if (key == NULL)
+	{
+		prints_error_msg(vars, ": No variable to unset");
+		return;
+	}
+	for (i = 0; vars->env[i] != NULL; i++)
+		;
+	newenv = malloc(sizeof(char *) * i);
+	if (newenv == NULL)
+	{
+		prints_error_msg(vars, NULL);
+		vars->status = 127;
+		new_exit(vars);
+	}
+	for (i = 0; vars->env[i] != *key; i++)
+		newenv[i] = vars->env[i];
+	for (j = i + 1; vars->env[j] != NULL; j++, i++)
+		newenv[i] = vars->env[j];
+	newenv[i] = NULL;
+	free(*key);
+	free(vars->env);
+	vars->env = newenv;
 	vars->status = 0;
 }
