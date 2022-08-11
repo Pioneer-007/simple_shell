245aaaada7fa3b08cd339d011cbecf48b2db7492
Oseni Sakariyau Oluwadamilare <oseni_sakariyau0008@yahoo.com>
2022-08-11 20:30:31 +0000

    fork_child.c

--- fork_child.c
+++ fork_child.c
@@ -91,3 +91,91 @@ char *path_finder(vars_t vars, char *command)
  * Return: Upon success returns the inde of the environmental variale
  * otherwise returns -1.
  */
+int find_env_index(vars_t vars, char *str)
+{
+	int i, len, j;
+
+	len = _strlen(str);
+	for (i = 0; vars.env[i] != NULL; i++)
+	{
+		for (j = 0; j < len; j++)
+		{
+			if (vars.env[i][j] != str[j])
+				break;
+		}
+		if (j == len && vars.env[i][j] == '=')
+			return (i);
+	}
+	return (-1);
+}
+
+/**
+ * tokenize_path - Separates a string of path as an array of
+ * strings containing the path directories.
+ * @vars: structure with variables will be used
+ * @index: index of the path in the environment variables.
+ * @str: string to separate and tokenize
+ * Return: On success a NLL terminated array of string.
+ * otherwise returns NULL.
+ */
+char **tokenize_path(vars_t vars, int index, char *str)
+{
+	char *env_var;
+	int token_count = 0, len;
+	char **path_tokens;
+	const char *delim = ":\n";
+
+	len = _strlen(str);
+
+	env_var = vars.env[index] + (len + 1);
+	path_tokens = token_interface(env_var, delim, token_count);
+	if (path_tokens == NULL)
+		return (NULL);
+
+	return (path_tokens);
+}
+
+/**
+ * search_directories - Looks through directories stored in path_tokens
+ * for a specific file aka command.
+ * @path_tokens: A pointer to array of strings containings de paths
+ * contained in the PATH environment variable.
+ * @command: Represents a command. i.e. ls, /bin/ls, pwd, etc.
+ *
+ * Return: On success a string with the directory containing the
+ * command file. Otherwise NULL.
+ */
+char *search_directories(char **path_tokens, char *command)
+{
+	int i, s;
+	char *cwd, *buf;
+	size_t size;
+	struct stat stat_buf;
+
+	buf = NULL;
+	size = 0;
+	cwd = getcwd(buf, size);
+	if (cwd == NULL)
+		return (NULL);
+	if (command[0] == '/')
+		command = command + 1;
+	for (i = 0; path_tokens[i] != NULL; i++)
+	{
+		s = chdir(path_tokens[i]);
+		if (s == -1)
+		{
+			perror("ERROR!");
+			return (NULL);
+		}
+		s = stat(command, &stat_buf);
+		if (s == 0)
+		{
+			chdir(cwd);
+			free(cwd);
+			return (path_tokens[i]);
+		}
+	}
+	chdir(cwd);
+	free(cwd);
+	return (NULL);
+}
