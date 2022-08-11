cd0be1e54bbdb4441922c684c21e3a2a2d27c712
Oseni Sakariyau Oluwadamilare <oseni_sakariyau0008@yahoo.com>
2022-08-11 19:48:25 +0000

    strtfunctions.c

--- strtfunctions.c
+++ strtfunctions.c
@@ -63,3 +63,68 @@ char *_strcat(char *strc1, char *strc2)
 
 	len1 = 0;
 	len2 = 0;
+	if (strc1 == NULL)
+		len1 = 0;
+	else
+	{
+		for (len1 = 0; strc1[len1]; len1++)
+			;
+	}
+	if (strc2 == NULL)
+		len2 = 0;
+	else
+	{
+		for (len2 = 0; strc2[len2]; len2++)
+			;
+	}
+	newlen = len1 + len2 + 1;
+	newstring = malloc(newlen * sizeof(char));
+	if (newstring == NULL)
+		return (NULL);
+	for (i = 0; i < len1; i++)
+		newstring[i] = strc1[i];
+	for (j = 0; j < len2; j++)
+		newstring[i + j] = strc2[j];
+	newstring[len1 + len2] = '\0';
+	return (newstring);
+}
+
+
+/*** WRITES STRING TO STDOUT ***/
+/**
+ * _puts - writes a string to standard output
+ * @str: string to write
+ *
+ * Return: number of chars printed or -1 on failure
+ */
+ssize_t _puts(char *str)
+{
+	ssize_t i, len;
+
+	for (i = 0; str[i]; i++)
+		;
+	len = write(1, str, i);
+	if (len != i)
+	{
+		perror("Fatal Error");
+		return (-1);
+	}
+	return (len);
+}
+
+/*** MEASURE THE LENGHT OF A STRING ***/
+/**
+ * _strlen - Calculates the lenght of a string.
+ * @str: String that needs length to be found.
+ * Return: Upon success returns the length of a string. otherwise 0.
+ */
+int _strlen(char *str)
+{
+	int i;
+
+	if (str == NULL)
+		return (0);
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	return (i);
+}
