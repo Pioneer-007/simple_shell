69bf3001ce893e40edd6f304850cb612ad858e6b
Oseni Sakariyau Oluwadamilare <oseni_sakariyau0008@yahoo.com>
2022-08-11 19:52:11 +0000

    simple_shell.c

--- simple_shell.c
+++ simple_shell.c
@@ -32,3 +32,36 @@ int main(int argc _attribute_((unused)), char **argv, char **environment)
 
 	vars.argv = argv;
 	vars.env = make_enviroment(environment);
+
+	signal(SIGINT, sig_handler);
+
+	if (!isatty(STDIN_FILENO))
+		is_pipe = 1;
+	if (is_pipe == 0)
+		_puts("$ ");
+	sig_flag = 0;
+
+	while (getline(&(vars.buffer), &len_buffer, stdin) != -1)
+	{
+		vars.counter++;
+		vars.commands = tokenizer(vars.buffer, ";");
+		for (i = 0; vars.commands && vars.commands[i] != NULL; i++)
+		{
+			vars.array_tokens = tokenizer(vars.commands[i], " \t\r\n\a");
+			if (vars.array_tokens && vars.array_tokens[0])
+				if (check_for_builtins(&vars) == NULL)
+				{
+					fork_child(vars);
+				}
+			free(vars.array_tokens);
+		}
+		free(vars.buffer);
+		free(vars.commands);
+		if (is_pipe == 0)
+			_puts("$ ");
+		vars.buffer = NULL;
+	}
+	free_env(vars.env);
+	free(vars.buffer);
+	exit(vars.status);
+}
