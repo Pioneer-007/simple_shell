6ad6e8d199ee353a5f29daf4cd6d89a72b606ba8
Oseni Sakariyau Oluwadamilare <oseni_sakariyau0008@yahoo.com>
2022-08-11 19:45:30 +0000

    tokenizer.c

--- tokenizer.c
+++ tokenizer.c
@@ -56,3 +56,61 @@ char **tokenize(int token_count, char *line, const char *delimiter)
 	if (buffer == NULL)
 		return (NULL);
 	token = new_strtok(line_cp, delimiter);
+	for (i = 0; token != NULL; i++)
+	{
+		buffer[i] = _strdup(token);
+		token = new_strtok(NULL, delimiter);
+	}
+	buffer[i] = NULL;
+	free(line_cp);
+	return (buffer);
+}
+
+/**
+ * token_interface - token interface
+ * @line: line get to be tokenized
+ * @delimiter: eny delimiter lie ; % " ", etc.
+ * @token_count: token counter.
+ * Return: tokens
+ **/
+char **token_interface(char *line, const char *delimiter, int token_count)
+{
+	vars_t vars;
+
+	token_count = count_token(line, delimiter);
+	if (token_count == -1)
+	{
+		free(line);
+		return (NULL);
+	}
+	vars.array_tokens = tokenize(token_count, line, delimiter);
+	if (vars.array_tokens == NULL)
+	{
+		free(line);
+		return (NULL);
+	}
+
+	return (vars.array_tokens);
+}
+
+/**
+ * count_token - token's count
+ * @line: string.
+ * @delimiter: delimiter
+ * Return: token's count
+ **/
+int count_token(char *line, const char *delimiter)
+{
+	char *str;
+	char *token;
+	int i;
+
+	str = _strdup(line);
+	if (str == NULL)
+		return (-1);
+	token = new_strtok(str, delimiter);
+	for (i = 0; token != NULL; i++)
+		token = new_strtok(NULL, delimiter);
+	free(str);
+	return (i);
+}
