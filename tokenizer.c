5ec4e7f4fb57c98961e81793fc6511fa8ba29603
Okogun Celina Blessing <cokogun@gmail.com>
2022-08-11 14:36:09 +0000

    tokenizer.c

---
+++ tokenizer.c
@@ -0,0 +1,58 @@
+#include "main.h"
+
+/**
+ * tokenizer - tokenizes a buffer with a delimiter
+ * @buffer: buffer to tokenize
+ * @delimiter: delimiter to tokenize along
+ *
+ * Return: pointer to an array of pointers to the tokens
+ */
+char **tokenizer(char *buffer, char *delimiter)
+{
+	char **tokens = NULL;
+	size_t i = 0, mcount = 10;
+
+	if (buffer == NULL)
+		return (NULL);
+	tokens = malloc(sizeof(char *) * mcount);
+	if (tokens == NULL)
+	{
+		perror("Fatal Error");
+		return (NULL);
+	}
+	while ((tokens[i] = new_strtok(buffer, delimiter)) != NULL)
+	{
+		i++;
+		if (i == mcount)
+		{
+			tokens = _realloc(tokens, &mcount);
+			if (tokens == NULL)
+			{
+				perror("Fatal Error");
+				return (NULL);
+			}
+		}
+		buffer = NULL;
+	}
+	return (tokens);
+}
+
+/**
+ * tokenize - tokenizes a buffer with a delimiter just use for for_child
+ * @line: buffer to tokenize
+ * @delimiter: delimiter to tokenize along
+ * @token_count: token count, size.
+ * Return: pointer to an array of pointers to the tokens
+ */
+char **tokenize(int token_count, char *line, const char *delimiter)
+{
+	int i;
+	char **buffer;
+	char *token;
+	char *line_cp;
+
+	line_cp = _strdup(line);
+	buffer = malloc(sizeof(char *) * (token_count + 1));
+	if (buffer == NULL)
+		return (NULL);
+	token = new_strtok(line_cp, delimiter);
