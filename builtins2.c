c969daaa4577969abe1785d40284c60045af065f
Okogun Celina Blessing <cokogun@gmail.com>
2022-08-11 15:06:13 +0000

    builtins2.c

---
+++ builtins2.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * new_cd - Changes the current working directory .
+ * if no parameter is passed it will change directory to HOME.
+ * @vars: A string representing the input from the user.
+ */
+void new_cd(vars_t *vars)
+{
+	int index;
